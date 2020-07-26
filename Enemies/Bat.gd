a35b3b81e7a16a6fba1039eb5109f9561da71291
John Adams <Johnadams@mail.usf.edu>
2020-07-26 18:29:45 -0400

    better bat AI

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -17,6 +17,7 @@ var knockback = Vector2.ZERO
 
 var state = CHASE
 
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 
@@ -35,7 +36,11 @@ func _physics_process(delta):
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-	
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+				
+			
 	velocity = move_and_slide(velocity)
 
 func seek_player():
